7cbf6552c60fda2bae6fe6d35b7f9905d7ea79df
ly <libyutil@gmail.com>
2021-03-05 01:13:42 +0000

    patched find module

--- find/FindCxxCliLib.cmake
+++ find/FindCxxCliLib.cmake
@@ -1,41 +1,31 @@
 
 cmake_minimum_required (VERSION 3.0.2)
 
-include(FindPackageHandleStandardArgs)
-
-if(NOT CXXCLI_FOUND)
+if(NOT CXXCLILIB_FOUND)
 
     find_package(Git REQUIRED)
 
     set(FIND_CxxCli_IMPORT_DIR "${CMAKE_BINARY_DIR}/CxxCli")
-    
-    macro(find_list_CxxCliLib)
-        find_path(
-            list_CxxCliLib
-            "CMakeLists.txt"
-            HINTS
-            "${FIND_CxxCli_IMPORT_DIR}"
-        )
-    endmacro()
+    set(FIND_CxxCli_BUILD_DIR "${CMAKE_BINARY_DIR}/CxxCli.build")
+    set(FIND_CxxCli_LISTFILE "${FIND_CxxCli_IMPORT_DIR}/CMakeLists.txt")
 
-    find_list_CxxCliLib()
-    if(EXISTS "${list_CxxCliLib}")
-        set(CXXCLI_DO_TESTS OFF CACHE STRING "CXXCLI_DO_TESTS" FORCE)
-        add_subdirectory("${FIND_CxxCli_IMPORT_DIR}" "${FIND_CxxCli_IMPORT_DIR}")
-    else()
+    if(NOT EXISTS "${FIND_CxxCli_LISTFILE}")
         file(MAKE_DIRECTORY "${FIND_CxxCli_IMPORT_DIR}")
         execute_process(
             COMMAND "${GIT_EXECUTABLE}" "clone" "https://github.com/LibYutil/CxxCli.git" "${FIND_CxxCli_IMPORT_DIR}"
         )
-
-        find_list_CxxCliLib()
-        if(EXISTS "${list_CxxCliLib}")
-            set(CXXCLI_DO_TESTS OFF CACHE STRING "CXXCLI_DO_TESTS" FORCE)
-            add_subdirectory("${FIND_CxxCli_IMPORT_DIR}" "${FIND_CxxCli_IMPORT_DIR}")
-        endif()
-
-        mark_as_advanced(FIND_CxxCli_IMPORT_DIR list_CxxCliLib)
-
     endif()
-    find_package_handle_standard_args(CxxCliLib DEFAULT_MSG list_CxxCliLib)
+    if(EXISTS "${FIND_CxxCli_LISTFILE}")
+        set(CXXCLI_DO_TESTS OFF CACHE STRING "CXXCLI_DO_TESTS" FORCE)
+        add_subdirectory("${FIND_CxxCli_IMPORT_DIR}" "${FIND_CxxCli_BUILD_DIR}")
+    endif()
+
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(CxxCliLib DEFAULT_MSG FIND_CxxCli_LISTFILE)
+
+    mark_as_advanced(
+        FIND_CxxCli_IMPORT_DIR
+        FIND_CxxCli_BUILD_DIR
+        FIND_CxxCli_LISTFILE
+    )
 endif()
